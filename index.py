585a8522209ba91d8cc2aac4b6e2f7e079384f3a
Muhammad <muhammadraza0047@gmail.com>
2018-11-23 02:08:17 -0500

    added mail functionality

--- index.py
+++ index.py
@@ -1,13 +1,14 @@
 import requests
 from flask import Flask , render_template
+from flask_mail import Mail, Message
 app = Flask(__name__)
 
-#hackernews api
+# hackernews api
 r = requests.get('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty')
 data = r.json()
 url = []
 title = []
-for x in range(0,50):
+for x in range(0,100):
 	news = data[x]
 	z = f'https://hacker-news.firebaseio.com/v0/item/{news}.json?print=pretty'
 	d = requests.get(z)
@@ -17,14 +18,31 @@ for x in range(0,50):
 			url.append(v['url'])
 			title.append(v['title'])
 		except KeyError:
-			print('went wrong')
+			
 
 json_data = dict(zip(title,url))
 # print(json_data)
 
-
+mail_settings = {
+    "MAIL_SERVER": 'YOUR STMP SERVER',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": '',
+    "MAIL_PASSWORD": ''
+}
+app.config.update(mail_settings)
+mail = Mail(app)
 @app.route("/")
-def hello():
-	return render_template("index.html",data=json_data.items())
+def email():
+	msg = Message(subject="Hacker News",
+	              sender=app.config.get("MAIL_USERNAME"),
+	              recipients=[""], # replace with your email for testing
+	              html=render_template('index.html',data=json_data.items()))
+	mail.send(msg)
+	return "sent"
+
 	
+
+
 
