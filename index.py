34e3b56373e4d49767ce777b963b2831b13c3c8c
Muhammad <muhammadraza0047@gmail.com>
2018-11-23 01:30:01 -0500

    added flask

--- index.py
+++ index.py
@@ -1,15 +1,30 @@
 import requests
-# hackernews api
+from flask import Flask , render_template
+app = Flask(__name__)
+
+#hackernews api
 r = requests.get('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty')
 data = r.json()
+url = []
+title = []
+for x in range(0,50):
+	news = data[x]
+	z = f'https://hacker-news.firebaseio.com/v0/item/{news}.json?print=pretty'
+	d = requests.get(z)
+	v = d.json()
+	if v['type'] == 'story':
+		try:
+			url.append(v['url'])
+			title.append(v['title'])
+		except KeyError:
+			print('went wrong')
 
-def hacker_news():
-	for x in range(0,30):
-		news = data[x]
-		z = f'https://hacker-news.firebaseio.com/v0/item/{news}.json?print=pretty'
-		d = requests.get(z)
-		v = d.json()
-		if v['type'] == 'story':
-			print(f'{x} ' + "Title: "+ v['title'] + '-' +"\n" + "url: "+v['url'])
+json_data = dict(zip(title,url))
+# print(json_data)
 
+
+@app.route("/")
+def hello():
+	return render_template("index.html",data=json_data.items())
+	
 
